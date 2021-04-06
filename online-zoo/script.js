6a29603242a1877b7d31cb93e627ec035a58b8eb
BertFrontEnd <Berton@yandex.ru>
2021-04-07 00:36:03 +0300

    fix: fix layouts, some styles and script

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -1,11 +1,14 @@
 const checkboxButton = document.querySelector('.header__checkbox');
 const theme = document.querySelector('html');
 const currentTheme = localStorage.getItem('theme');
+const checkbox = document.querySelector('.checkbox__switcher');
+const indicator = document.querySelector('.checkbox__indicator');
+const checkedState = localStorage.getItem('state');
 
-function setTheme(name) {
+const setTheme = (name) => {
   theme.setAttribute('data-background', name);
   localStorage.setItem('theme', name);
-}
+};
 
 if (currentTheme) {
   theme.setAttribute('data-background', currentTheme);
@@ -13,10 +16,23 @@ if (currentTheme) {
   setTheme('light');
 }
 
+const setState = (x) => {
+  indicator.style.left = `${x}px`;
+  localStorage.setItem('state', x);
+};
+
+if (checkedState) {
+  indicator.style.left = `${checkedState}px`;
+} else {
+  setState('6');
+}
+
 checkboxButton.addEventListener('change', () => {
   if (theme.getAttribute('data-background') === 'light') {
     setTheme('dark');
+    setState('28');
   } else {
     setTheme('light');
+    setState('6');
   }
 });
